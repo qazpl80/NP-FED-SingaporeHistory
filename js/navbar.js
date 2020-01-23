113c1894ec6c5b53535a7270a60e2c3c65d30ca2
Ling Hui Gladys Chua <s10196678@connect.np.edu.sg>
2020-01-23 23:33:58 +0800

    grgd

---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+
+        $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+
+        $('.animated-icon3').toggleClass('open');
+    });
+});

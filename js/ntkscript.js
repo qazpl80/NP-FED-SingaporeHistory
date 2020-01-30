9b1faf4be32e6051fb4decf0d5ba7d2a9498060c
Tze Keat Ng <s10195448@connect.np.edu.sg>
2020-01-30 19:33:42 +0800

    push by me lol

---
+++ js/ntkscript.js
@@ -0,0 +1,16 @@
+$(function(){  
+  $('.monster').fadeIn('slow');
+});
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_object ){                
+                $(this).animate({'opacity':'1'},1500);                    
+            }            
+        });     
+    });    
+});

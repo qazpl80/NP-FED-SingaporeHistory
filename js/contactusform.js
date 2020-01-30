12c5bcebca97960ddb1b17255fff74bc7759b7a0
Yuan Ming Tan <s10198319@connect.np.edu.sg>
2020-01-30 22:00:27 +0800

    eee

--- js/contactusform.js
+++ js/contactusform.js
@@ -1,11 +1,11 @@
 // JavaScript source code
 function submitForm() {
     var divobj = document.getElementById("message");
-    divobj.style.display = "block";
-    divobj.innerHTML = ("Your request have been submitted.");
+    alert("Your request have been submitted.\nWe will contact you through your e-mail provided")
 }
 
 function resetForm() {
     var freset = document.getElementById("form");
     freset.reset();
+    divobj.style.display = 'none';
 }

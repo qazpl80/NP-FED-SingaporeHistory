836f3bd0020786ea4873b7992b6088dc9eb998b9
Zhe Yu Chua <s10196605@connect.np.edu.sg>
2020-01-31 22:21:18 +0800

    Removed extra stuff

--- js/quiz.js
+++ js/quiz.js
@@ -30,39 +30,21 @@ function CalculateScore() {
 function submitquiz() {
     var score = CalculateScore();
     if (score == 0) {
-        var divobj = document.getElementById("message");
-        divobj.style.display = "block";
-        divobj.innerHTML = "0/5"
         alert("You got " + score + " out of 5. Please reread the article and try again!");
     }
     else if (score == 1) {
-        var divobj = document.getElementById("message");
-        divobj.style.display = "block";
-        divobj.innerHTML = "1/5"
-        alert("You got " + score + " out of 5. You can do better!");
+       alert("You got " + score + " out of 5. You can do better!");
     }
     else if (score == 2) {
-        var divobj = document.getElementById("message");
-        divobj.style.display = "block";
-        divobj.innerHTML = "2/5"
         alert("You got " + score + " out of 5. You can defiently do better by reading the article and finding the correct answers");
     }
     else if (score == 3) {
-        var divobj = document.getElementById("message");
-        divobj.style.display = "block";
-        divobj.innerHTML = "3/5"
         alert("You got " + score + " out of 5. You passed but there is definetly room for improvement");
     }
     else if (score == 4) {
-        var divobj = document.getElementById("message");
-        divobj.style.display = "block";
-        divobj.innerHTML = "4/5"
         alert("You got " + score + " out of 5. Almost there! Maybe one careless mistake");
     }
     else if (score == 5) {
-        var divobj = document.getElementById("message");
-        divobj.style.display = "block";
-        divobj.innerHTML = "5/5"
         alert("You got " + score + " out of 5. congratulations you got everything correct, shows that you read the article well");
     }
 
